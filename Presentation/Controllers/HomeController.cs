83ca2d6d080e4282b92bbe7d3cbef7338b4c051f
Pham Nghi <nghipham2003@gmail.com>
2025-05-05 09:05:26 +0700

    Add project files.

---
+++ Presentation/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult _PartialLogin()
+        {
+            return PartialView();
+        }
+        public ActionResult _PartialRegister()
+        {
+            return PartialView();
+        }
+        public ActionResult _PartialFooter()
+        {
+            return PartialView();
+        }
+        public ActionResult _PartialMenu()
+        {
+            return PartialView();
+        }
+        public ActionResult _PartialPlayer()
+        {
+            return PartialView();
+        }
+        public ActionResult _PartialHeader()
+        {
+            return PartialView();
+        }
+        public ActionResult RecentPlay()
+        {
+            return PartialView();
+        }
+        public ActionResult WeekTop()
+        {
+            return PartialView();
+        }
+        public ActionResult Release()
+        {
+            return PartialView();
+        }
+        public ActionResult TopGenres()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Presentation/Scripts/style.js
@@ -0,0 +1,85 @@
+﻿$(function () { 
+    $('.play-left-arrow').click(function (e) { 
+        e.preventDefault();
+
+        $('.player_left').toggleClass('open-list');
+
+        const isOpen = $('.player_left').hasClass('open-list');
+
+        $('.type_playlist').toggleClass('reordered', isOpen);
+        
+    });
+
+    $('.queue .que_button').click(function (e) { 
+        e.preventDefault();
+        
+        $('.playlist-wrap').addClass('queue-open');
+    });
+
+    $('.queue-top .queue-close i').click(function (e) { 
+        e.preventDefault();
+        
+        $('.playlist-wrap').removeClass('queue-open');
+    });
+
+    $('.player_close').click(function (e) { 
+        e.preventDefault();
+        
+        $('.player_wrapper').toggleClass('player_down');
+    });
+
+    $('.nav-close').click(function (e) { 
+        e.preventDefault();
+        
+        $('.side-menu').toggleClass('open_nav');
+    });
+
+    $('.right-button .close-sidebar').click(function (e) { 
+        e.preventDefault();
+        
+        $('.side-menu').toggleClass('open_nav');
+    });
+    $('.more_icon').click(function (e) {
+        e.preventDefault();
+
+        // Lấy thẻ <ul> tương ứng với nút được click
+        var $currentOption = $(this).siblings('.more-option');
+
+        // Đóng tất cả các danh sách đang mở, trừ danh sách tương ứng
+        $('.more-option').not($currentOption).removeClass('xh-block-more');
+
+        // Toggle danh sách tương ứng
+        $currentOption.toggleClass('xh-block-more');
+    });
+
+
+    $('.album_btn .play_btn').click(function (e) { 
+        e.preventDefault();
+        
+        $('.play-song').toggleClass('pause-song');
+    });
+
+
+    // $('.nav-wrapper ul li').click(function (e) { 
+    //     e.preventDefault();
+        
+    //     $('.nav-wrapper li').removeClass('active');
+    //     $(this).addClass('active');
+
+    // });
+    const toggleButton = document.getElementById('themeToggle');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme === 'dark') {
+        $('body').addClass('dark-mode');
+    }
+
+    toggleButton.addEventListener('click', function () {
+        $('body').toggleClass('dark-mode');
+        const theme = $('body').hasClass('dark-mode') ? 'dark' : 'light';
+        localStorage.setItem('theme', theme);
+    });
+
+    
+ });
+ 
